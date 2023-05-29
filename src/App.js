242a5a9e63d5e60b1f08c68be47857011a140b5a
AriParvin <arian.parvin@parvin.de>
2023-05-29 08:51:45 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+		<h1>ACHSO</h1>
+        <p>This page is under construction!</p>
+        <a
+          className="App-link"
+          href="https://mehyarabboud.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mehyars Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
